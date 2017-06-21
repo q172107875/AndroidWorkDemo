260e2a8852554f0a68b68c64c44f122d9070e8cc
qb <172107875@qq.com>
2017-06-21 14:32:55 +0800

    1

--- app/src/main/java/qibo/org/myandroiddemo/MainActivity.java
+++ app/src/main/java/qibo/org/myandroiddemo/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 locationAndContactsTask();
                 break;
             case R.id.fragment:
-
                 break;
         }
     }
