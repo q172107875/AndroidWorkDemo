44ccb5e386a104eadaf2c4c7567f30d64799ef65
qb <172107875@qq.com>
2017-06-21 13:39:01 +0800

    测试项目

---
+++ app/src/main/java/qibo/org/myandroiddemo/MainActivity.java
@@ -0,0 +1,85 @@
+package qibo.org.myandroiddemo;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener ,EasyPermissions.PermissionCallbacks{
+    private static final String TAG = "MainActivity";
+    private Button button_camera;
+    private Button button_location_and_contacts;
+    private Button fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        button_camera = (Button) findViewById(R.id.button_camera);
+        button_location_and_contacts = (Button) findViewById(R.id.button_location_and_contacts);
+        fragment = (Button) findViewById(R.id.fragment);
+
+        button_camera.setOnClickListener(this);
+        button_location_and_contacts.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_camera:
+                cameraTask();
+                break;
+            case R.id.button_location_and_contacts:
+                locationAndContactsTask();
+                break;
+            case R.id.fragment:
+
+                break;
+        }
+    }
+
+
+    public void cameraTask() {
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)) {
+            // Have permission, do the thing!
+            Toast.makeText(this, "TODO: Camera things", Toast.LENGTH_LONG).show();
+        } else {
+            // Ask for one permission
+            EasyPermissions.requestPermissions(this, "否则怎样1",
+                    123, Manifest.permission.CAMERA);
+        }
+    }
+
+    public void locationAndContactsTask() {
+        String[] perms = { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_CONTACTS,Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE };
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Have permissions, do the thing!
+            Toast.makeText(this, "TODO: Location and Contacts things", Toast.LENGTH_LONG).show();
+        } else {
+            // Ask for both permissions
+            EasyPermissions.requestPermissions(this, "否则怎样2",
+                    124, perms);
+        }
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        Log.d(TAG, "onPermissionsGranted:" + requestCode + ":" + perms.size());
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "onPermissionsDenied:" + requestCode + ":" + perms.size());
+    }
+}
